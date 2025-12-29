ebbaae940cd1779363911ba630db8287dd369f6a
nikesh chauhdari <info.nikesh12@gmail.com>
2025-12-29 23:49:02 +0545

    blog

--- dashboard.php
+++ dashboard.php
@@ -7,12 +7,15 @@ print_r($_SESSION);
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Dashboard</title>
 </head>
+
 <body>
     <h1>Admin Dashboard</h1>
 </body>
+
 </html>
