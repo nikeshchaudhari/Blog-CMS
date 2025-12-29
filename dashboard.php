8023f348fa9ecbb549b497c62c525a39e95f616d
nikesh chauhdari <info.nikesh12@gmail.com>
2025-12-29 23:37:59 +0545

    blog

---
+++ dashboard.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+
+print_r($_SESSION);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Dashboard</title>
+</head>
+<body>
+    <h1>Admin Dashboard</h1>
+</body>
+</html>
