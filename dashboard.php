41f5a26f243d0d5c4d9b03e236e573ee7702b43a
nikesh chauhdari <info.nikesh12@gmail.com>
2025-12-30 22:33:10 +0545

    blog

--- dashboard.php
+++ dashboard.php
@@ -1,6 +1,9 @@
 <?php
 session_start();
-
+if(!isset($_SESSION['user'])){
+    header("Location: login.php");
+    exit();
+}
 
 
 ?>
@@ -19,16 +22,16 @@ session_start();
 
 <body>
    <!-- sidebar -->
-    <div class="sidebar shadow-lg w-[250px]  h-screen flex flex-col p-2">
-        <h2 class="text-[20px] text-center ">Admin</h2>
-        <a href="dashboard.php">Dashboard</a>
+    <div class="sidebar shadow-lg w-[250px]  h-screen flex flex-col m-2">
+        <h2 class="text-[20px] text-center font-bold mb-2 ">Welcome <?php echo $_SESSION['user'] ?></h2>
+        <a href="dashboard.php" class="  bg-red-500 p-2">Dashboard</a>
         <a href="#">Posts</a>
         <a href="#">Users</a>
         <a href="#">Setting</a>
         <a href="logout.php">Logout</a>
     </div>
 <div class="main">
-    
+
 </div>
 </body>
 
