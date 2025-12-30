296774856e706a9596a8a5f825a61f0240b9b217
nikesh chauhdari <info.nikesh12@gmail.com>
2025-12-30 21:37:14 +0545

    blog

--- dashboard.php
+++ dashboard.php
@@ -12,10 +12,21 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Dashboard</title>
+        <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css">
+
 </head>
 
 <body>
-    <h1>Admin Dashboard</h1>
+   <!-- sidebar -->
+    <div class="sidebar">
+        <h2>Admin</h2>
+        <a href="dashboard.php">Dashboard</a>
+        <a href="#">Posts</a>
+        <a href="#">Users</a>
+        <a href="#">Setting</a>
+        <a href="#">Logout</a>
+    </div>
 </body>
 
 </html>
