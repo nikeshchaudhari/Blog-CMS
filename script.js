0881c596ccee1547b1833564433d17229f5d3e58
nikesh chauhdari <info.nikesh12@gmail.com>
2026-01-02 00:35:50 +0545

    blog

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 // dashboard-sidebar
 
 const menu_links = document.querySelectorAll(".menu-link")
-console.log(menu_links);
+// console.log(menu_links);
 const currentPage = window.location.pathname.split('/').pop();
-console.log(currentPage);
+// console.log(currentPage);
 menu_links.forEach(menu_links=>{
     if(menu_links.getAttribute("href")===currentPage){
         menu_links.classList.add("active");
     }
 })
 
+// mobile-slider
 
+const overlay = document.querySelector("#overlay")
+const hamburger = document.querySelector("#hamburger")
+const slider = document.querySelector(".mobile-slider")
 
-
+hamburger.addEventListener("click",()=>{
+    slider.classList.remove("-left-64")
+    slider.classList.add("left-0")
+    overlay.classList.remove("-left-64")
+})
