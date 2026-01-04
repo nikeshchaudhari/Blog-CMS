b105d509b974e9218e8736c79dc27534f84fe0f8
nikesh chauhdari <info.nikesh12@gmail.com>
2026-01-04 23:11:00 +0545

    blog

--- admin.php
+++ admin.php
@@ -5,15 +5,30 @@ $username = "admin";
 $password = "qwerty";
 
 $hash = password_hash($password,PASSWORD_DEFAULT);
+$check_admin_query = "SELECT id FROM users WHERE username='$username"
+$data = mysqli_query($conn,$check_admin_query);
+$result = mysqli_fetch_assoc($data);
+if($result>0){
+    echo "admin already register";
+}
 
-$query = "INSERT INTO users(username,password)VALUES('$username','$hash')";
-$data = mysqli_query($conn, $query);
-if($data){
+// insert query
+
+$query = "INSERT INTO users(username,password,role)VALUES($username,$hash,'admin')";
+$admin_data = mysqli_query($conn,$query);
+if($admin_data){
     ?>
-    <script>
-        alert("Data Insert");
-    </script>
+
+<script>
+    alert("Admin Add Successfully...");
+</script>
+<?php
+}else{
+    ?>
+<script>
+    alert("Admin Not Added ...");
+</script>
     <?php
-}    
+}
 
 ?>
