b743a3ac71c5a46e4858c611a3e2ce9511624ae8
nikesh chauhdari <info.nikesh12@gmail.com>
2026-01-05 22:18:03 +0545

    blog

--- post.php
+++ post.php
@@ -8,7 +8,7 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Blog || Admin Dashboard</title>
+    <title>Blog || Post Dashboard</title>
         <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css">
 
