68c5e5ea28095c6baca982fee9c4e44ac28717cf
nikesh chauhdari <info.nikesh12@gmail.com>
2026-01-07 21:12:11 +0545

    blog

---
+++ register.php
@@ -0,0 +1,34 @@
+<?php ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog || Register</title>
+        <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+
+</head>
+<body>
+    <main class="w-full h-screen flex justify-center bg-gray-200/100">
+        <div class="w-[400px] h-[450px] m-auto bg-white/20  shadow-xl p-2 rounded">
+            <h2 class="text-center text-[25px] font-bold">Register</h2>
+            <form method="post" class="flex flex-col m-2">
+                <label for="username" class="mb-2">FullName</label>
+                <input type="text" id="username" name="full_name" placeholder="Enter FullName" class="border p-2 rounded mb-2 ">
+                
+                <label for="email" class="mb-2">Email</label>
+                <input type="email" id="email" name="email" placeholder="Enter Email" class="border p-2 rounded mb-4">
+                 <label for="password" class="mb-2">Password</label>
+                <input type="password" id="password" name="password" placeholder="Create New Password" class="border p-2 rounded mb-2 ">
+                   <label for="password" class="mb-2">Confirm Password</label>
+                <input type="password" id="password" name="password" placeholder="Confirm Password" class="border p-2 rounded mb-2">
+                <button type="submit" name="login-btn" class="bg-gray-800 p-2 text-white rounded-lg hover:bg-gray-700 cursor-pointer">Register</button>
+            </form>
+            <?php 
+           
+            ?>
+        </div>
+    </main>
+</body>
+</html>
