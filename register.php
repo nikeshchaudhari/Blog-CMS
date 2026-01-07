6172d54bdc44b62e13d2a4232f7b675ed10c7d61
nikesh chauhdari <info.nikesh12@gmail.com>
2026-01-07 22:47:12 +0545

    blog

--- register.php
+++ register.php
@@ -32,34 +32,37 @@ $data = mysqli_query($conn,$query);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Blog || Register</title>
         <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
-
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css">
 </head>
 <body>
     <main class="w-full h-screen flex justify-center bg-gray-200/100">
         <div class="w-[400px] h-[450px] m-auto bg-white/20  shadow-xl p-2 rounded">
             <h2 class="text-center text-[25px] font-bold">Register</h2>
             <form method="post" class="flex flex-col m-2">
-                <div class="flex flex-col">
+                <div class="flex flex-col ">
                     <label for="username" class="mb-2">FullName</label>
                 <input type="text" id="username" name="full_name" placeholder="Enter FullName" class="border p-2 rounded mb-2 ">
+               
                 </div>
         
               <div  class="flex flex-col">
                   <label for="email" class="mb-2">Email</label>
                 <input type="email" id="email" name="email" placeholder="Enter Email" class="border p-2 rounded mb-4">
               </div>
-                <div  class="flex flex-col">
+                <div  class="flex flex-col relative">
                      <label for="password" class="mb-2">Password</label>
                 <input type="password" id="password" name="password" placeholder="Create New Password" class="border p-2 rounded mb-2 ">
+                <i class="fa-regular fa-eye absolute right-3 top-11 cursor-pointer"></i>     
                 </div>
-                 <div  class="flex flex-col">
+                 <div  class="flex flex-col relative">
                       <label for="conpassword" class="mb-2">Confirm Password</label>
                 <input type="password" id="conpassword" name="confirmpassword" placeholder="Confirm Password" class="border p-2 rounded mb-4">
-                 </div>
+<i class="fa-regular fa-eye absolute right-3 top-11 cursor-pointer"></i>                 </div>
                 <button type="submit" name="register-btn" class="bg-gray-800 p-2 text-white rounded-lg hover:bg-gray-700 cursor-pointer">Register</button>
             </form>
           
         </div>
     </main>
+    <script src="script.js"></script>
 </body>
 </html>
