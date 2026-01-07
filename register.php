d194937f6357ece9f926cea080a8a8a323e0bd3c
nikesh chauhdari <info.nikesh12@gmail.com>
2026-01-07 22:24:57 +0545

    blog

--- register.php
+++ register.php
@@ -10,12 +10,12 @@ if(isset($_POST["register-btn"])){
     // check empty
     if($fullName==""||$email==""||$password==""||$confirmPassword==""){
         echo "All fields are required";
-        exit;
+        // exit;
     }
     // check match
     if($password !== $confirmPassword){
         echo "password doesnot match";
-        exit;
+        // exit;
     }
 
     $query="INSERT INTO user(fullname,email,password)VALUES('$fullName','$email','$hash')";
@@ -39,15 +39,23 @@ $data = mysqli_query($conn,$query);
         <div class="w-[400px] h-[450px] m-auto bg-white/20  shadow-xl p-2 rounded">
             <h2 class="text-center text-[25px] font-bold">Register</h2>
             <form method="post" class="flex flex-col m-2">
-                <label for="username" class="mb-2">FullName</label>
+                <div class="flex flex-col">
+                    <label for="username" class="mb-2">FullName</label>
                 <input type="text" id="username" name="full_name" placeholder="Enter FullName" class="border p-2 rounded mb-2 ">
+                </div>
         
-                <label for="email" class="mb-2">Email</label>
+              <div  class="flex flex-col">
+                  <label for="email" class="mb-2">Email</label>
                 <input type="email" id="email" name="email" placeholder="Enter Email" class="border p-2 rounded mb-4">
-                 <label for="password" class="mb-2">Password</label>
+              </div>
+                <div  class="flex flex-col">
+                     <label for="password" class="mb-2">Password</label>
                 <input type="password" id="password" name="password" placeholder="Create New Password" class="border p-2 rounded mb-2 ">
-                   <label for="password" class="mb-2">Confirm Password</label>
-                <input type="password" id="password" name="confirmpassword" placeholder="Confirm Password" class="border p-2 rounded mb-4">
+                </div>
+                 <div  class="flex flex-col">
+                      <label for="conpassword" class="mb-2">Confirm Password</label>
+                <input type="password" id="conpassword" name="confirmpassword" placeholder="Confirm Password" class="border p-2 rounded mb-4">
+                 </div>
                 <button type="submit" name="register-btn" class="bg-gray-800 p-2 text-white rounded-lg hover:bg-gray-700 cursor-pointer">Register</button>
             </form>
           
