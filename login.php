844328c44549c1084ef2371f205c72926430bb62
nikesh chauhdari <info.nikesh12@gmail.com>
2026-01-08 16:34:17 +0545

    blog

--- login.php
+++ login.php
@@ -18,8 +18,8 @@ include("config.php");
         <div class="w-[400px] h-[280px] m-auto bg-white/20  shadow-xl p-2 rounded">
             <h2 class="text-center text-[25px] font-bold">Login</h2>
             <form method="post" class="flex flex-col m-2">
-                <label for="username" class="mb-2">Username</label>
-                <input type="text" id="username" name="username" placeholder="Enter username" class="border p-2 rounded mb-2 ">
+                <label for="email" class="mb-2">Email</label>
+                <input type="text" id="email" name="username" placeholder="Enter email" class="border p-2 rounded mb-2 ">
                 
                 <label for="pass" class="mb-2">Password</label>
                 <input type="password" id="pass" name="password" placeholder="Enter password" class="border p-2 rounded mb-4">
@@ -27,30 +27,16 @@ include("config.php");
             </form>
             <?php 
             if(isset($_POST["login-btn"])){
-                $username=trim($_POST["username"]);
-                $password=trim($_POST["password"]);
+               $email = $_POST["email"];
+               $password =$_POST["password"];
 
                 
 
-                $query = "SELECT * FROM users WHERE username ='$username'";
+                $query = "SELECT * FROM user WHERE email ='$email'";
                 $data = mysqli_query($conn, $query);    
 
                 $result = mysqli_fetch_assoc($data);
-                     
-                if($result && password_verify($password,$result['password'])){
-                    $_SESSION['user']=$username;
-                    $_SESSION['role']=$result['role'];
-
-                    // role redirect
-
-                   $route = ($result['role']=='admin')? 'dashboard.php ':'index.php';
-                   header("Location: $route");
-                   exit();
-
-                  
-                }else{
-                    echo " username and password inscorrect";
-                };
+               
         } 
             ?>
         </div>
