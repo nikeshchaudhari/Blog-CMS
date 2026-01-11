ea1d63bb3b47d920c5ac4295342b230a591e3395
nikesh chauhdari <info.nikesh12@gmail.com>
2026-01-11 14:15:41 +0545

    blog

--- logout.php
+++ logout.php
@@ -1,7 +1,23 @@
 <?php
 
 session_start();
+
+// 1. Unset all session variables
+$_SESSION = array();
+
+// 2. Kill the session cookie in the browser
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
 session_destroy();
+// if(!isset($_SESSION["email"])){
+//     header("Location: login.php");
+//     exit();
+// }
 
 header("Location: login.php");
 exit();
